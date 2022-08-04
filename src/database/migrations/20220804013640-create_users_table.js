21fd3b53badf3a17bf0f239882d98bb154bc7e7f
BraianMaynard <Braian_maynard@hotmail.com>
2022-08-03 22:39:43 -0300

    dataBase

---
+++ src/database/migrations/20220804013640-create_users_table.js
@@ -0,0 +1,23 @@
+//up para correr migracion y down para hacer un rolback 
+
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};

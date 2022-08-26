3bba0f5dbf43c04c2521c478b1ca32221304bcc8
BraianMaynard <Braian_maynard@hotmail.com>
2022-08-25 23:45:03 -0400

    seeders

---
+++ src/database/seeders/20220826032707-product.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('products', [
+      {
+        
+      }
+        ], {});
+    },
+   
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+     await queryInterface.bulkDelete('products', null, {});
+  }
+};
---
+++ src/database/seeders/20220826033125-user.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+     await queryInterface.bulkInsert('users', [
+      {
+         
+        
+         }
+        ], {});
+   
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+     await queryInterface.bulkDelete('users', null, {});
+  }
+};

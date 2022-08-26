c4e9705bb4fdd596a47770e334b9752b49b15b0f
BraianMaynard <Braian_maynard@hotmail.com>
2022-08-25 23:12:40 -0400

    login

--- src/controllers/loginControllers.js
+++ src/controllers/loginControllers.js
@@ -1,7 +1,14 @@
+const User = require ('../models/User')
+
 const controller = {
     index: (req, res) => {
-        res.render("login")
+        res.render("login") 
+    },
+
+    loginProcess: (req, res) =>{
+        let userToLogin = User 
     }
+
 }
 
 module.exports = controller;

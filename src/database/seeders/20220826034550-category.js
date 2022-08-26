3c8a23bbbd7fd5795a297e29b21c920316b25393
BraianMaynard <Braian_maynard@hotmail.com>
2022-08-25 23:52:03 -0400

    seeders

---
+++ src/database/seeders/20220826034550-category.js
@@ -0,0 +1,41 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await queryInterface.bulkInsert('category', [
+      {
+        name: '',
+      },
+      {
+        name: '',
+      },
+      {
+        name: '',
+      },
+      {
+        name: '',
+      }
+
+
+    ], {});
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('category', null, {});
+  }
+};

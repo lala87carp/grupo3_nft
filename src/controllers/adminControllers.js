83911bf9d3111db815937f73574a8fd036a25ce5
BraianMaynard <Braian_maynard@hotmail.com>
2022-09-03 15:43:51 -0300

    admin

--- src/controllers/adminControllers.js
+++ src/controllers/adminControllers.js
@@ -1,11 +1,19 @@
 const path = require('path');
 const fs = require('fs');
+const db = require('../database/models');
+
 
 const controller = {
-    index: (req, res) => {
-        let productDataBase = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../Data/productDataBase.json')));
-        res.render(path.resolve(__dirname, '../Views/admin'), {productDataBase});
-    },
+    index: async (req, res) => {
+        const products = await db.Product.findAll()
+        
+        res.render('./admin', { products });
     }
+    // index: (req, res) => {
+    //     let productDataBase = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../Data/productDataBase.json')));
+    //     res.render(path.resolve(__dirname, '../Views/admin'), {productDataBase});
+    // },
+    }
+
 
 module.exports = controller;

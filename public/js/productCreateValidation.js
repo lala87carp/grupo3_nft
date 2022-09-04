3a6c0271513f0d073f9ea397ecf9e63687a1a05b
Maggie Gonzalez <margaritagonzalezp00@gmail.com>
2022-09-04 20:24:15 -0300

    validations front create y edit

--- public/js/productCreateValidation.js
+++ public/js/productCreateValidation.js
@@ -1,25 +1,37 @@
-const form = document.getElementById('createForm');
-const nombreCreate = document.getElementById('nombreCreate');
-const imageCreate = document.getElementById('imageCreate');
-const descripcionCreate = document.getElementById('descripcionCreate');
-
 
 window.addEventListener("load", function () {
-    let formulario = document.querySelector("form.reservation"); //chequear nombre
+    let formulario = document.getElementById("createForm"); //chequear nombre
     formulario.addEventListener("submit", function (e) {
         e.preventDefault();
 
         let errores = []
         
+        const nombreCreate = document.getElementById('nombreCreate');
         if (nombreCreate.value == " ") {
             errores.push("El campo de nombre tiene que estar completo")
         } else if (nombreCreate.value.length < 5) {
             errores.push("El campo de nombre debe tener al menos 5 caracteres");
         }
 
+        const descripcionCreate = document.getElementById('description');
         if (descripcionCreate.value.length < 20) {
             errores.push("El campo de nombre debe tener al menos 5 caracteres");
         }; 
+        
+
+        const file = document.getElementById('image');
+        function validate_fileupload(fileName) {
+            var allowed_extensions = ["jpg", "png", "gif", "jpeg"];
+            var file_extension = fileName.split('.').pop().toLowerCase(); // split function will split the filename by dot(.), and pop function will pop the last element from the array which will give you the extension as well. If there will be no extension then it will return the filename.
+
+            var isValid = allowed_extensions.includes(file_extension);
+            return isValid;
+        }
+        if(!validate_fileupload(file.value)){
+            errores.push("El tipo de archivo no es válido debe ser jpg, png, gif o jpeg")
+        }
+
+
         if (errores.length > 0) {
             e.preventDefault();
 
@@ -29,6 +41,8 @@ window.addEventListener("load", function () {
                 erroresUl.innerHTML += "<li>" + errores[i] + "</li>";
             };
         };
+
+
     });
     })
 
--- public/js/productEditValidation.js
+++ public/js/productEditValidation.js
@@ -1,25 +1,34 @@
-const form = document.getElementById('createForm');
-const nombreEdit = document.getElementById('nombreEdit');
-const imageEdit = document.getElementById('imageEdit');
-const descripcionEdit = document.getElementById('descripcionEdit');
-
-
 window.addEventListener("load", function () {
-    let formulario = document.querySelector("form.reservation"); //chequear nombre
+    let formulario = document.getElementById("editForm"); //chequear nombre
     formulario.addEventListener("submit", function (e) {
         e.preventDefault();
 
         let errores = []
         
+        const nombreEdit = document.getElementById('nombreEdit');
         if (nombreEdit.value == " ") {
             errores.push("El campo de nombre tiene que estar completo")
         } else if (nombreEdit.value.length < 5) {
             errores.push("El campo de nombre debe tener al menos 5 caracteres");
         }
 
+        const descripcionEdit = document.getElementById('descripcionEdit');
         if (descripcionEdit.value.length < 20) {
-            errores.push("El campo de nombre debe tener al menos 5 caracteres");
+            errores.push("El campo de descripción debe tener al menos 20 caracteres");
         }; 
+
+        const file = document.getElementById('imageEdit');
+        function validate_fileupload(fileName) {
+            var allowed_extensions = ["jpg", "png", "gif", "jpeg"];
+            var file_extension = fileName.split('.').pop().toLowerCase(); // split function will split the filename by dot(.), and pop function will pop the last element from the array which will give you the extension as well. If there will be no extension then it will return the filename.
+
+            var isValid = allowed_extensions.includes(file_extension);
+            return isValid;
+        }
+        if(!validate_fileupload(file.value)){
+            errores.push("El tipo de archivo no es válido debe ser jpg, png, gif o jpeg")
+        }
+
         if (errores.length > 0) {
             e.preventDefault();
 
